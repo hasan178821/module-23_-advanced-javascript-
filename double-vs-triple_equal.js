5be7dcdbc8e51b2c3b1a33807ae1461048be68a1
hasan178821 <hasanrana178821@gmail.com>
2021-02-02 01:19:02 +0600

    double-vs-triple_equal done

---
+++ double-vs-triple_equal.js
@@ -0,0 +1,15 @@
+const num1 = 3;
+const num2 = "3";
+if (num1 == num2) {
+    console.log("the condition is true");
+} else {
+    console.log("the condition is false");
+}
+
+const amount1 = 3;
+const amount2 = "3";
+if (amount1 === amount2) {
+    console.log("the conditio is true");
+} else {
+    console.log("the condition is false");
+}
