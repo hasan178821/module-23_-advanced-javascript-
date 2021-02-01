047226a2e6fd7329f5907e33f72954d28d7a6f06
hasan178821 <hasanrana178821@gmail.com>
2021-02-02 00:36:01 +0600

    true false done

---
+++ true-false.js
@@ -0,0 +1,14 @@
+// falsy 
+// 0, "", undefined, null, NaN, false
+
+// truthy
+// "0", " ", [], {}
+
+
+
+const age = [];
+if (age) {
+    console.log("the conditions is true");
+} else {
+    console.log("the condition is false");
+}
