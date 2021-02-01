cc64ea3af6baac3e4f2aa554b69603dceb0956f8
hasan178821 <hasanrana178821@gmail.com>
2021-02-02 01:02:42 +0600

    null-vs-undefined done

---
+++ null-vs-undefined.js
@@ -0,0 +1,19 @@
+// undefined-----
+let ages;
+console.log(ages);
+
+function name(num1, num2) {
+    const sum = num1 + num2;
+    return
+}
+const result = name(20, 40);
+console.log(result);
+
+function number(num1, num2) {
+    const sum = num1 + num2;
+}
+const result2 = number(20);
+console.log(result2);
+
+let ages = [33, 23, 35, 14];
+console.log(ages[9]);
